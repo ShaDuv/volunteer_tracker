900ce3726fe971ec253627a7c52dfe5ca9c2cf02
Shawna Duvall <shawnaduvall@gmail.com@>
2019-08-02 13:13:05 -0700

    will pass test 10. Took a lot longe because apparently I have been running the wrong version of Ruby this entire time due to a typo when I first installed it. Fixed.

--- lib/project.rb
+++ lib/project.rb
@@ -1,9 +1,14 @@
 class Project
   attr_reader :id
   attr_accessor :title
+
 def initialize(attr)
   @title = attr.fetch(:title)
   @id = attr.fetch(:id)
   end
 
+  def save
+      result = DB.exec("INSERT INTO projects (title) VALUES ('#{@title}') RETURNING id;")
+      @id = result.first().fetch("id").to_i
+    end
 end
