d140044114778575c19579be7b32810a1c191f35
Shawna Duvall <shawnaduvall@gmail.com@>
2019-08-02 15:08:17 -0700

    pass test 17

--- lib/project.rb
+++ lib/project.rb
@@ -41,4 +41,9 @@ def initialize(attr)
   def volunteers
     Volunteer.find_by_project(self.id)
   end
+
+  def update(attributes)
+  @title = attributes.fetch(:title)
+  DB.exec("UPDATE projects SET title = '#{@title}' WHERE id = #{@id};")
+  end
 end
